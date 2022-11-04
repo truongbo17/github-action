3240b6349cedc63753f252f9471d560b32996162
Nguyễn Quang Trường <truongnq@d2t.vn>
2022-11-04 11:26:12 +0700

    update yaml file

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log('Test Github Actions');
 console.log('Test Change Github Actions');
 console.log('Workflow');
 console.log('Change from branch feature_update');
+console.log('Change from branch feature_update');
