b2564aba554d5699334b3d085745244dd6d51afd
Nguyễn Quang Trường <truongnq@d2t.vn>
2022-11-04 11:57:35 +0700

    update

--- app.js
+++ app.js
@@ -2,3 +2,7 @@ console.log('Test Github Actions');
 console.log('Test Change Github Actions');
 console.log('Workflow');
 console.log('Change from branch feature_update');
+
+console.log(1 + 1);
+console.log(2 * 2);
+console.log(9 / 3);
